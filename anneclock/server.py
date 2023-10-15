8d4151c1668c1cfafca153902b7498dc08b574aa
Chris Cantrell <topherCantrell@gmail.com>
2023-10-15 06:30:37 -0500

    First deployed version!

--- anneclock/server.py
+++ anneclock/server.py
@@ -1,25 +1,130 @@
 import aiohttp
 import aiohttp.web
 import json
+import socket
+import os
+import time
+import subprocess
 
-async def cmd_update(message):
-    print('IN UPDATE')
-    return {}
+CONFIG_INFO = {
+    "ssid": None,
+    "psk": None,
+    "net_connected": None,
+    "software_version": "1.0", # Fixed here in the source
+    "latest_version": None,
+}
+
+async def cmd_check_update(ws, message):
+    latest_version = _check_upgrade()
+    print('>>>',latest_version)
+    msg = {
+        'cmd': 'latest_version',        
+    }    
+    if latest_version == CONFIG_INFO['software_version']:
+        msg['info'] = f'Up to date (version {latest_version})'
+        msg['color'] = 'green'
+    else:
+        msg['info'] = f'New version {latest_version} available'
+        msg['color'] = 'orange'    
+    await ws.send_str(json.dumps(msg))    
+
+async def cmd_update(cmd, message):
+    _run_upgrade()
+
+async def cmd_set_network(cmd, message):
+    print('>>>', cmd)
+    print('>>>', message)
+    ssid = message['ssid']
+    psk = message['psk']
+    _set_network(ssid, psk)
+    os.system('reboot')
+    print('>>> REBOOTING')
+
+def _set_network(ssid, psk):
+
+    file_contents = '''ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+network={
+        ssid="%SSID%"
+        psk="%PSK%"
+}
+'''
+    # TODO create/update our section in the file in case there are other nets
+    file_contents = file_contents.replace('%SSID%',ssid)
+    file_contents = file_contents.replace('%PSK%',psk)
+    with open('/etc/wpa_supplicant/wpa_supplicant.conf','w') as f:
+        f.write(file_contents)
+        
+
+def _check_upgrade():
+    try:
+        try:
+            os.system('rm /home/pi/update.py')
+        except Exception:
+            pass
+        os.system('wget https://raw.githubusercontent.com/topherCantrell/anne-clock/main/anneclock/update.py -O /home/pi/update.py')
+        with open('/home/pi/update.py') as f:
+            lines = f.readlines()
+        latest_version = lines[0].strip()[2:]
+        return latest_version
+    except Exception:
+        return None
+    
+def _run_upgrade():
+    _check_upgrade()  # This downloads the file
+    os.system('python3 /home/pi/update.py')
+
+
+def _get_network():
+    try:
+        with open('/etc/wpa_supplicant/wpa_supplicant.conf') as f:
+            file_contents = f.read()    
+        file_contents = file_contents.split('\n')
+        # TODO parse this correctly
+        ssid = file_contents[4]
+        psk = file_contents[5]
+        i = ssid.find('"')
+        j = ssid.find('"',i+1)
+        ssid = ssid[i+1:j]
+        i = psk.find('"')
+        j = psk.find('"',i+1)
+        psk = psk[i+1:j]
+        return ssid, psk
+    except:
+        return '',''
+    
+def _is_network_connected():
+    for i in range(10):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(("github.com",80))
+            s.close()
+            return True
+        except:
+            # We retru here
+            time.sleep(1)
+    return False
 
 async def websocket_handler(request):    
     ws = aiohttp.web.WebSocketResponse()
     await ws.prepare(request)
 
-    async for msg in ws:    
-        print('>>>',type(msg.data), f'::{msg.data}::'  ) 
-        cmd_msg = json.loads(msg.data)
-        
+    msg = dict(CONFIG_INFO)
+    msg['cmd'] = 'CONFIG_INFO'
+    await ws.send_str(json.dumps(msg))    
+
+    async for msg in ws:            
+        cmd_msg = json.loads(msg.data)        
         cmd = cmd_msg['cmd']        
-        if cmd=='update':
-            resp = await cmd_update(cmd_msg)        
+        if cmd=='run_update':
+            await cmd_update(cmd, cmd_msg)       
+        elif cmd=='set_network':
+            await cmd_set_network(cmd, cmd_msg) 
+        elif cmd=='check_update':
+            await cmd_check_update(ws, cmd_msg)
         else:
-            resp = {'error':f'Unknown command: {cmd}'}
-        await ws.send_str(json.dumps(resp))
+           print('UNKNOWN COMMAND',cmd_msg)
     return ws
 
 """
@@ -37,7 +142,14 @@ Responses are JSON with "error" only if something is wrong
 
 """
 
-print('>>> here')
+wifi_info = _get_network()
+CONFIG_INFO['ssid'] = wifi_info[0]
+CONFIG_INFO['psk'] = wifi_info[1]
+CONFIG_INFO['net_connected'] = _is_network_connected()
+CONFIG_INFO['latest_version'] = _check_upgrade()
+
+print('>>> CONFIG_INFO')
+print(CONFIG_INFO)
 
 app = aiohttp.web.Application()
 app.router.add_route('GET', '/ws', websocket_handler)
--- anneclock/webroot/clock.js
+++ anneclock/webroot/clock.js
@@ -9,15 +9,109 @@ KioskBoard.run('.js-kioskboard-input', {})
 
 main_buttons_shown = false
 
+
 BACK_IMAGES = [
-    'back06.jpg',
-    'back02.jpg',
-    'back03.jpg',
-    'back04.jpg',
-    'back05.jpg',
-    'back01.jpg',
+    {
+        'name':'back01.jpg',   
+        'digital_time': [450, 20],
+        'digital_date': [670, 600],       
+        'clock_color': '#cd1515',
+        'month_color': '#cd1515',
+        'day_of_week_color': '#cd1515',
+        'date_color':'#cd1515'
+    },
+    {
+        'name':'back02.jpg',  
+        'digital_time': [60, 20],
+        'digital_date': [670, 300],           
+        'clock_color': 'purple',
+        'month_color': 'purple',
+        'day_of_week_color': 'purple',
+        'date_color':'purple'      
+    },
+    {
+        'name':'back03.jpg',         
+        'digital_time': [80, 80],
+        'digital_date': [0, 600],          
+        'clock_color': 'whitesmoke',
+        'month_color': 'whitesmoke',
+        'day_of_week_color': 'whitesmoke',
+        'date_color':'whitesmoke'  
+    },
+    {
+        'name':'back04.jpg',  
+        'digital_time': [40, 40],
+        'digital_date': [10, 580],        
+        'clock_color': 'darkblue',
+        'month_color': 'darkblue',
+        'day_of_week_color': 'darkblue',
+        'date_color':'darkblue'      
+    },
+    {
+        'name':'back05.jpg',    
+        'digital_time': [20, 0],
+        'digital_date': [670, 580],        
+        'clock_color': 'whitesmoke',
+        'month_color': 'whitesmoke',
+        'day_of_week_color': 'whitesmoke',
+        'date_color':'whitesmoke'     
+    },
+    {
+        'name':'back06.jpg',                 
+        'digital_time': [20, 0],
+        'digital_date': [670, 600],                
+        'clock_color': 'purple',
+        'month_color': 'purple',
+        'day_of_week_color': 'purple',
+        'date_color':'purple'     
+    },    
+    {
+        'name':'back08.jpg',   
+        'digital_time': [20, 600],
+        'digital_date': [670, 600],    
+        'clock_color': 'blue',
+        'month_color': 'red',
+        'day_of_week_color': 'red',
+        'date_color':'red'      
+    },
+    {
+        'name':'back09.jpg',        
+        'digital_time': [600, 160],
+        'digital_date': [600, 500],        
+        'clock_color': 'whitesmoke',
+        'month_color': 'whitesmoke',
+        'day_of_week_color': 'whitesmoke',
+        'date_color':'whitesmoke'
+    },
+    {
+        'name':'back10.jpg',     
+        'digital_time': [660, 0],
+        'digital_date': [0, 600],       
+        'clock_color': 'whitesmoke',
+        'month_color': 'whitesmoke',
+        'day_of_week_color': 'whitesmoke',
+        'date_color':'whitesmoke'   
+    },
+    {
+        'name':'back11.jpg',        
+        'digital_time': [660, 450],
+        'digital_date': [660, 600],       
+        'clock_color': 'whitesmoke',
+        'month_color': 'whitesmoke',
+        'day_of_week_color': 'whitesmoke',
+        'date_color':'whitesmoke'   
+    },
+    {
+        'name':'back12.jpg',       
+        'digital_time': [0, 600],
+        'digital_date': [660, 600],        
+        'clock_color': '#c1233b',
+        'month_color': '#c1233b',
+        'day_of_week_color': '#c1233b',
+        'date_color':'#c1233b'    
+    },
 ]
-BACK_IMAGE_NUM = 0
+BACK_IMAGE_NUM = -1
 
 AUDIO_CLIPS = [
     'clip1.mp3',
@@ -59,12 +153,20 @@ function bt_config(event) {
     b = document.getElementById('config')
     b.style.display = 'block'
     a.style.display = 'none'
+    a = document.getElementById('clock_digital_time')
+    b = document.getElementById('clock_digital_date')
+    b.style.display = 'none'
+    a.style.display = 'none'
 }
 function bt_cfg_done(event) {
     a = document.getElementById('config')
     b = document.getElementById('main')
     b.style.display = 'block'
     a.style.display = 'none'
+    a = document.getElementById('clock_digital_time')
+    b = document.getElementById('clock_digital_date')
+    b.style.display = 'block'
+    a.style.display = 'block'
 }
 
 function bt_audio(event) {
@@ -90,13 +192,34 @@ function bt_video(event) {
 }
 
 function bt_background(event) {
-    event.stopPropagation()
-    b = document.getElementById('main')
+    if(event) {
+        event.stopPropagation()
+    }
+    a = document.getElementById('main')
     BACK_IMAGE_NUM = BACK_IMAGE_NUM+1
     if(BACK_IMAGE_NUM>=BACK_IMAGES.length) {
         BACK_IMAGE_NUM = 0
     }
-    b.style.backgroundImage="url('backgrounds/back0"+(BACK_IMAGE_NUM+1)+".jpg')"
+    info = BACK_IMAGES[BACK_IMAGE_NUM]
+    a.style.backgroundImage="url('backgrounds/"+(BACK_IMAGES[BACK_IMAGE_NUM]['name'])
+    a = document.getElementById('clock_digital_time')
+    a.style.left = info['digital_time'][0].toString()+'px'
+    a.style.top = info['digital_time'][1].toString()+'px'
+    a = document.getElementById('clock_digital_date')
+    a.style.left = info['digital_date'][0].toString()+'px'
+    a.style.top = info['digital_date'][1].toString()+'px'
+
+    a = document.getElementById('digital_time')
+    a.style.color = info['clock_color']
+    a = document.getElementById('digital_am_pm')
+    a.style.color = info['clock_color']
+    a = document.getElementById('digital_day_of_week')
+    a.style.color = info['day_of_week_color']
+    a = document.getElementById('digital_month')
+    a.style.color = info['month_color']
+    a = document.getElementById('digital_date')
+    a.style.color = info['date_color']
+    
 }
 
 active_tab = 'wifi'
@@ -123,11 +246,122 @@ function send_message(msg) {
     ws.send(JSON.stringify(msg))
 }
 
+bt_background()
+
 var ws = new WebSocket("ws:/"+window.location.host+"/ws")
 
 ws.onopen = function() {}
 
 ws.onmessage = function(evt) {
     msg = JSON.parse(evt.data)
-    // alert("got back:"+msg['error'])
+    if (msg['cmd'] == 'CONFIG_INFO') {      
+        console.log('CONFIG'+JSON.stringify(msg))
+        a = document.getElementById('cfg_wifi_ssid')
+        a.value = msg['ssid']
+        b = document.getElementById('cfg_wifi_psk')  
+        b.value = msg['psk']        
+        c = document.getElementById('cfg_wifi_status')        
+        if(msg['net_connected'] === true) {
+            c.textContent = 'Connected'
+            c.style.color = 'green'
+        } else if(msg['net_connected'] === false) {
+            c.textContent = 'Not Connected'
+            c.style.color = 'red'
+        } else {
+            c.textContent = '--unknown--'
+            c.style.color = 'red'
+        }
+    } else if (msg['cmd']=='latest_version') {
+        a = document.getElementById('cfg_update_status')
+        a.textContent = msg['info']
+        a.style.color = msg['color']
+    } else {
+        console.log('Unknown message from server:'+JSON.stringify(msg))
+    }
 }
+
+function bt_cfg_connect() {
+    a = document.getElementById('cfg_wifi_ssid')
+    b = document.getElementById('cfg_wifi_psk')
+    msg = {
+        'cmd': 'set_network',
+        'ssid': a.value,
+        'psk': b.value
+    }
+    send_message(msg)
+}
+
+function bt_check_update() {
+    msg = {
+        'cmd': 'check_update'
+    }
+    send_message(msg)
+}
+function bt_run_update() {
+    msg = {
+        'cmd': 'run_update'
+    }
+    send_message(msg)
+}
+
+const NAMES_WEEKDAY = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const NAMES_MONTH = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+current_date_time = ""
+
+function updateDateTime() {
+    const date = new Date()
+    month = date.getMonth() // 0-11
+    day_number = date.getDate() // 1-31
+    day_name = date.getDay() // 0-6 (0=Sunday)
+    time_hour = date.getHours() // 0-23
+    time_minute = date.getMinutes() // 0-59 (make this print 2 digits)
+    is_pm = false
+    if (time_hour>=12) {
+        time_hour = time_hour - 12
+        is_pm = true
+    }
+    if (time_hour==0) {
+        time_hour = 12
+    }
+    // to strings
+    month = NAMES_MONTH[month]
+    day_number = day_number.toString()
+    day_name = NAMES_WEEKDAY[day_name]        
+    time_hour = time_hour.toString()
+    time_minute_org = time_minute
+    if(time_minute<10) {
+        time_minute = "0"+time_minute.toString()
+    } else {
+        time_minute = time_minute.toString()
+    }  
+    if(is_pm) {
+        is_pm = "PM"
+    } else {
+        is_pm = "AM"
+    }
+    new_date_time = month+day_number+day_name+time_hour+time_minute+is_pm
+    if(new_date_time != current_date_time) {        
+        current_date_time = new_date_time        
+        // update fields
+        document.getElementById('digital_time').textContent = time_hour+':'+time_minute
+        document.getElementById('digital_am_pm').textContent = is_pm
+        //
+        document.getElementById('digital_day_of_week').textContent = day_name
+        document.getElementById('digital_month').textContent = month
+        document.getElementById('digital_date').textContent = day_number
+        //
+        if((time_minute%10)==0) {
+            // Every 10 minutes
+            bt_background()
+        }
+        if(time_minute==0) {
+            // On the hour
+            bt_audio()
+        }        
+    }
+
+}
+
+updateDateTime()
+window.setInterval(updateDateTime, 1000*10);
